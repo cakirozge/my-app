af500c1ff5e2a8d58d573d607d3dd93cda0570d5
Ozge Cakir <caakir.ozge@gmail.com>
2024-01-01 15:34:01 +0300

    product card

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { ProductModel } from '../../models/response/ProductModel';
+import { Link } from 'react-router-dom';
 
-type Props = {}
+type Props = {
+  product:ProductModel;
+  title?:string; //opsiyonel
+}
 
 const ProductCard = (props: Props) => {
   return (
-    <div>ProductCard</div>
+    <div className="card" >
+    <img src={props.product.thumbnail} className="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">{props.product.title}</h5>
+      <p className="card-text"> {props.product.description} </p>
+      <Link to={"/product-detail?id=" + props.product.id} className='btn btn-primary'  >Details</Link>
+      <button className='btn btn-danger'>Sil</button>
+    </div>
+  </div>
+    
   )
 }
 
 export default ProductCard
+
+
+//product?: ? işareti ile opsiyonel hale getirdik.
