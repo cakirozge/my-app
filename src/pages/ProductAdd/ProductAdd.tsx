95558bcb0a6534addfeea0ffecd0b99cc2eeadeb
Ozge Cakir <caakir.ozge@gmail.com>
2024-01-02 14:02:36 +0300

    formik

--- src/pages/ProductAdd/ProductAdd.tsx
+++ src/pages/ProductAdd/ProductAdd.tsx
@@ -1,48 +1,56 @@
+import { Formik, Form, Field} from 'formik';
 import React from 'react'
 
 type Props = {}
 
 const ProductAdd = (props: Props) => {
+
+  const initialValues = {
+    title:"",
+    description:"",
+    price:0,
+    stok:0,
+  }
+
+// Form içindeki DİV İSİM veriyoruz örn title.
+//Böylece değişken olmaktan cıkıp, bir objeye dönüşürler.
+//onSubmit = "Handler" görünce fonksiyon olduğunu anlamalıyız.
+//afee + tab ile arrow func. kısaltması elde ederim.
+
+//Fieldları value bağlamalıyız.
+//Field içindeki name alanına oluşturduğumuz ilgili initialValueları yazmalıyız. --backend ismine göre yap.
+
+//onSubmit={(value) => {console.log(value); value ve console ilişkisi direkt json olarak yazdırır.
+
   return (
-    <div className="container">
-      <form>
+    <div className="container mt-5">
+      <Formik initialValues={initialValues} onSubmit={(value) => {console.log(value);}}> 
+      <Form>
         <div className="mb-3">
           <label className="form-label">Ürün Adı</label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
+          <Field
+           name ="title" type="text" className="form-control"
           />
         </div>
 
         <div className="mb-3">
           <label className="form-label">Ürün Açıklaması</label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
+          <Field
+          name ="description" type="text" className="form-control"
           />
         </div>
 
         <div className="mb-3">
           <label className="form-label">Ürün Fiyatı</label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
+          <Field
+            name ="price" type="number" className="form-control"
           />
         </div>
 
         <div className="mb-3">
           <label className="form-label">Ürün Stok</label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
+          <Field
+           name ="stok" type="number" className="form-control"
           />
         </div>
 
@@ -51,7 +59,8 @@ const ProductAdd = (props: Props) => {
         <button type="submit" className="btn btn-primary">
           Kayıt
         </button>
-      </form>
+      </Form>
+      </Formik>
     </div>
   );
 }
@@ -59,3 +68,9 @@ const ProductAdd = (props: Props) => {
 export default ProductAdd;
 
 //for sildik.
+//Komplex formları yönetmek için FORMIK Kulanacağız.
+//formu formik ile sarmalladık ve form baş harfini büyüttük.
+//Formik :  initialValues, onSubmitts ister bunları vermeliyiz.
+
+//Formik içindeki form alanının Field diye kullandığımız ismi Formikin Field ile değiştirmeliyiz.
+//saf elemente ulaşsın diye.
